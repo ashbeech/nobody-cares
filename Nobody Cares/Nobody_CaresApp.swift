0378d894600d882e45d891c9bcac2e21dd71e085
Ash Beech <ash@bullish.design>
2026-02-11 12:30:20 +0000

    Initial Commit

---
+++ Nobody Cares/Nobody_CaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nobody_CaresApp.swift
+//  Nobody Cares
+//
+//  Created by Ashley Davison on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Nobody_CaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nobody CaresTests/Nobody_CaresTests.swift
@@ -0,0 +1,17 @@
+//
+//  Nobody_CaresTests.swift
+//  Nobody CaresTests
+//
+//  Created by Ashley Davison on 11/02/2026.
+//
+
+import Testing
+@testable import Nobody_Cares
+
+struct Nobody_CaresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
